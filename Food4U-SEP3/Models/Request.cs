83c019864c4750d26ad5f666228ba12f71fcda33
RalGoX1 <293346@viauc.dk>
2021-11-11 10:30:36 +0100

    Updated to Socket

---
+++ Food4U-SEP3/Models/Request.cs
@@ -0,0 +1,8 @@
+namespace Food4U_SEP3.Models
+{
+    public class Request
+    {
+        public string Type { get; set; }
+        public string Context { get; set; }
+    }
+}
--- Food4U-SEP3/UserServices/UserService.cs
+++ Food4U-SEP3/UserServices/UserService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Food4U_SEP3.Models;
@@ -17,6 +19,8 @@ namespace Food4U_SEP3.UserServices
         //private readonly FileContext fileContext;
         
         private string uri = "http://localhost:8080/api/v1/";
+        
+        private TcpClient tcpClient = new TcpClient("127.0.0.1", 2910);
 
         public UserService()
         {
@@ -33,6 +37,7 @@ namespace Food4U_SEP3.UserServices
 
         public async Task <User> ValidateLogin(string username)
         {
+            /*
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync($"{uri}users/{username}");
             if (response.StatusCode == HttpStatusCode.OK)
@@ -42,6 +47,7 @@ namespace Food4U_SEP3.UserServices
                 return resultUser;
             }
             return null;
+            */
         
             /*
             User first = Users.FirstOrDefault(x => x.Username.Equals(username));
@@ -50,6 +56,28 @@ namespace Food4U_SEP3.UserServices
             if (first != null) throw new Exception("User already exists");
             return first;
             */
+            
+            NetworkStream stream = tcpClient.GetStream();
+            
+            Request newRequest = new Request
+            {
+                Type = "ValidateUser",
+                Context = username
+            };
+            
+            string serialisedRequest = JsonSerializer.Serialize(newRequest);
+            byte[] dataToServer = Encoding.ASCII.GetBytes(serialisedRequest);
+            stream.Write(dataToServer, 0, dataToServer.Length);
+            
+            byte[] fromServer = new byte[1024];
+            int bytesRead = stream.Read(fromServer, 0, fromServer.Length);
+            string response = Encoding.ASCII.GetString(fromServer, 0, bytesRead);
+            Console.WriteLine(response);
+            
+            User resultUser = JsonSerializer.Deserialize<User>(response);
+            return resultUser;
+            
+            
         }
     }
     }
