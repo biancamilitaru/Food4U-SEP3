06c5edde18262a650656053daeb8fa3c5f231c94
kyratolnai11 <266375@viauc.dk>
2021-11-22 13:13:12 +0100

    Method for editing restaurant info

--- Tier1/Data/RestaurantService/IRestaurantService.cs
+++ Tier1/Data/RestaurantService/IRestaurantService.cs
@@ -7,5 +7,6 @@ namespace Client.Data.RestaurantService
     public interface IRestaurantService
     {
         Task AddRestaurantAsync(Restaurant restaurant);
+        Task EditRestaurantAsync(Restaurant restaurant);
     }
 }
