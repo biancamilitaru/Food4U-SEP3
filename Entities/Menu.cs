395b76de2966cfd9b70d01eb6c5859a17599229f
Tolnai <tolnai.milan22@gmail.com>
2021-11-29 10:57:34 +0100

    Menu model

---
+++ Entities/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Entities
+{
+    public class Menu
+    {
+        [Required, JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+}
