e85250c0ea8a3a019306253379f0241adacc84ff
Tolnai <tolnai.milan22@gmail.com>
2021-11-29 14:12:52 +0100

    Created Socket connection in Tier 2

---
+++ Tier2/SocketHandler/ISocketCategoryHandlerT2.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Entities;
+
+namespace Food4U_SEP3.SocketHandler
+{
+    public interface ISocketCategoryHandlerT2
+    {
+        Task<Category> AddCategory(Category category);
+
+        Task<Category> UpdateCategory(Category category, int categoryId);
+    }
+}
