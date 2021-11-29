c10752eaf6e8fd25e6f7c15f2aacfc50e4de5b5e
Tolnai <tolnai.milan22@gmail.com>
2021-11-29 13:48:11 +0100

    Created Category modell

---
+++ Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Entities
+{
+    public class Category
+    {
+        [JsonPropertyName("categoryID")]
+        public int CategoryId { get; set; }
+        
+        [Key, Required, JsonPropertyName("name")]
+        public string Name { get; set; }
+        
+        [JsonPropertyName("menuID")]
+        public int MenuId { get; set; }
+    }
+}
