32e380630ead3f5a57fe01b7e6f44e21716c8517
kyratolnai11 <266375@viauc.dk>
2021-12-03 11:01:31 +0100

    SocketHandler T2 for adding customer

---
+++ Tier2/SocketHandler/CustomerHandler/CustomerSocketHandlerT2.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Food4U_SEP3.SocketHandler.CustomerHandler
+{
+    public class CustomerSocketHandlerT2 :SocketHandler, ICustomerHandler
+    {
+        public Task<Customer> AddCustomer(Customer customer)
+        {
+            string customerAsJson = JsonSerializer.Serialize(customer);
+            SendToServer("AddRestaurant",customerAsJson);
+            return Task.FromResult(customer);
+        }
+    }
+}
---
+++ Tier2/SocketHandler/CustomerHandler/ICustomerHandlerT2.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Entities;
+
+namespace Food4U_SEP3.SocketHandler.CustomerHandler
+{
+    public interface ICustomerHandler
+    {
+        Task<Customer> AddCustomer(Customer customer);
+    }
+}
