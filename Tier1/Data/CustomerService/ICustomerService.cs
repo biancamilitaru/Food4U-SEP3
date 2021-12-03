7199e3defd79979a694088f81e257f5480475063
Tolnai <tolnai.milan22@gmail.com>
2021-12-03 11:05:42 +0100

    Service classes for Customer in Tier1

---
+++ Tier1/Data/CustomerService/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Entities;
+
+namespace Client.Data.CustomerService
+{
+    public interface ICustomerService
+    {
+        Task AddCustomerAsync(Customer customer);
+    }
+}
