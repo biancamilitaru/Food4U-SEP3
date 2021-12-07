2e4aacac0fb74bcb0ac3e3bfb492c6208555fce5
Tolnai <tolnai.milan22@gmail.com>
2021-12-07 11:11:36 +0100

    Service classes for Order in Tier 2

---
+++ Tier2/Service/OrderService/OrderServiceT2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Food4U_SEP3.Service.OrderService
+{
+    public class OrderServiceT2 : IOrderServiceT2
+    {
+        private readonly IOrderServiceT2 orderServiceT2;
+        
+        public async Task<Order> AddOrderAsync(Order order)
+        {
+            try
+            {
+                return await orderServiceT2.AddOrderAsync(order);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
