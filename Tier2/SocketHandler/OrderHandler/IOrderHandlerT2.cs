5f6d7ab6fcf6746c671fe129f5bb7034a23c0a66
PeterBlasko <peeterblahaa@gmail.com>
2021-12-09 10:45:24 +0100

    get previous orders Socket order handler

--- Tier2/SocketHandler/OrderHandler/IOrderHandlerT2.cs
+++ Tier2/SocketHandler/OrderHandler/IOrderHandlerT2.cs
@@ -8,13 +8,12 @@ namespace Food4U_SEP3.SocketHandler.OrderHandler
     public interface IOrderHandlerT2
     {
         Task<Order> AddOrder(Order order);
-
-        Task<List<Order>> GetIncomingOrders(string restaurantUsername);
-
         Task<Order> UpdateOrder(Order order);
         
         Task<Order> DeleteOrder(int orderId);
 
         Task<IList<Order>> GetAcceptedOrders(string restaurantUsername);
+        Task<List<Order>> GetIncomingOrders(string restaurantUsername);
+        Task<IList<Order>> GetPreviousOrders(string customerUsername);
     }
 }
