b0ba5354e8f406ae052ce9b3c626f5ff344047eb
RalGoX1 <293346@viauc.dk>
2021-12-09 11:04:58 +0100

    Send notification with order status.

--- Tier2/Service/OrderService/OrderServiceT2.cs
+++ Tier2/Service/OrderService/OrderServiceT2.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Entities;
+using Food4U_SEP3.SocketHandler;
+using Food4U_SEP3.SocketHandler.CustomerHandler;
 using Food4U_SEP3.SocketHandler.OrderHandler;
 
 namespace Food4U_SEP3.Service.OrderService
@@ -9,10 +11,16 @@ namespace Food4U_SEP3.Service.OrderService
     public class OrderServiceT2 : IOrderServiceT2
     {
         private readonly IOrderHandlerT2 orderHandlerT2;
+        private readonly ICustomerHandlerT2 customerHandlerT2;
+        private readonly IEmailService emailService;
+        private readonly IRestaurantHandlerT2 restaurantHandlerT2;
         
-        public OrderServiceT2(IOrderHandlerT2 orderHandlerT2)
+        public OrderServiceT2(IOrderHandlerT2 orderHandlerT2, IEmailService emailService, ICustomerHandlerT2 customerHandlerT2, IRestaurantHandlerT2 restaurantHandlerT2)
         {
             this.orderHandlerT2 = orderHandlerT2;
+            this.emailService = emailService;
+            this.customerHandlerT2 = customerHandlerT2;
+            this.restaurantHandlerT2 = restaurantHandlerT2;
         }
         
         public async Task<Order> AddOrderAsync(Order order)
@@ -58,6 +66,17 @@ namespace Food4U_SEP3.Service.OrderService
             try
             {
                 return await orderHandlerT2.UpdateOrder(order);
+                Order editedOrder = await orderHandlerT2.UpdateOrder(order);
+                Customer customer = await customerHandlerT2.GetCustomer(order.CustomerUsername);
+                Restaurant restaurant = await restaurantHandlerT2.GetRestaurant(order.RestaurantUsername);
+                if (editedOrder != null)
+                {
+                    emailService.SendEmail("Order info - " + restaurant.Name, 
+                        "There is a change in your order. Your status has been updated to,<\\br>"+
+                        "Status of your order: " +order.Status,
+                        customer.Email);    
+                }
+                return editedOrder;
             }
             catch (Exception e)
             {
