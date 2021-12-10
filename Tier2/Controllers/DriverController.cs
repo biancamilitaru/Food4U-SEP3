df6e073929a13d68525688b9978ca2fd750422c0
kyratolnai11 <266375@viauc.dk>
2021-12-10 09:19:03 +0100

    Driver Controller finished

--- Tier2/Controllers/DriverController.cs
+++ Tier2/Controllers/DriverController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Entities;
+using Food4U_SEP3.SocketHandler.DriverHandler;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Food4U_SEP3.Controllers
 {
@@ -7,5 +11,19 @@ namespace Food4U_SEP3.Controllers
     
     public class DriverController : ControllerBase
     {
+        private readonly IDriverHandlerT2 driverHandlerT2;
+        
+        [HttpPost]
+        public async Task<ActionResult<Driver>> AddDriverAsync([FromBody] Driver driver)
+        {
+            try
+            {
+                await driverHandlerT2.AddDriver(driver);
+                return Created($"/{driver}", driver);
+            } catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
     }
 }
