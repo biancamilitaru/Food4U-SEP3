86df59682fff859a9ecd616f0bbd010d6ccec6dc
kyratolnai11 <266375@viauc.dk>
2021-12-10 08:56:44 +0100

    driver controller

---
+++ Tier2/Controllers/DriverController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Food4U_SEP3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    
+    public class DriverController : ControllerBase
+    {
+    }
+}
