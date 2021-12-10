385f245571bc777118351e5d4e017415bc423ddb
Tolnai <tolnai.milan22@gmail.com>
2021-12-10 09:18:18 +0100

    Log in for Driver in Service classes in T2

--- Tier2/Service/DriverService/IDriverServiceT2.cs
+++ Tier2/Service/DriverService/IDriverServiceT2.cs
@@ -6,6 +6,8 @@ namespace Food4U_SEP3.Service.DriverService
     public interface IDriverServiceT2
     {
         Task<Driver> AddDriverAsync(Driver driver);
-        
+
+        Task<Driver> ValidateLoginAsync(string username);
+
     }
 }
