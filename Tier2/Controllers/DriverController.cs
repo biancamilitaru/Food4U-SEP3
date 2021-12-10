01b4fc680008f090c3e62d92e14a7ebaa701c9b9
PeterBlasko <peeterblahaa@gmail.com>
2021-12-10 09:27:26 +0100

    Driver controller validate login

--- Tier2/Controllers/DriverController.cs
+++ Tier2/Controllers/DriverController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Entities;
+using Food4U_SEP3.Service.DriverService;
 using Food4U_SEP3.SocketHandler.DriverHandler;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,16 +12,31 @@ namespace Food4U_SEP3.Controllers
     
     public class DriverController : ControllerBase
     {
-        private readonly IDriverHandlerT2 driverHandlerT2;
+        private readonly IDriverServiceT2 driverService;
+        
+        public DriverController(IDriverServiceT2 driverService) => this.driverService = driverService;
         
         [HttpPost]
         public async Task<ActionResult<Driver>> AddDriverAsync([FromBody] Driver driver)
         {
             try
             {
-                await driverHandlerT2.AddDriver(driver);
+                await driverService.AddDriverAsync(driver);
                 return Created($"/{driver}", driver);
             } catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<Driver>> GetDriverAsync([FromQuery] string username)
+        {
+            try
+            {
+                Driver driver = await driverService.ValidateLoginAsync(username);
+                return Ok(driver);
+            }
+            catch (Exception e)
             {
                 return StatusCode(500, e.Message);
             }
